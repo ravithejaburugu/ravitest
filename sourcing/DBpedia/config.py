2c1b8debc7df1430a9ac68e54eb9f0e3ac512f0f
Anshul Ghogre <30301360+aghogre@users.noreply.github.com>
2017-08-11 16:30:12 +0530

    Arguments and Config.py

---
+++ sourcing/DBpedia/config.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 11 13:16:53 2017
+
+@author: Admin
+"""
+
+
+import os
+
+
+argument_config = {
+    'azure_account_name': os.getenv('AZURE_ACCOUNT_NAME', 'randomtrees'),
+    'azure_account_key': os.getenv('AZURE_ACCOUNT_KEY', 'wvNLlB2cSHhB0OFPRhIQDv+1QBJ1CnwFt+AGfQnL8rTyKTCG90t1Z+aCepe25aol6CKneJYgvHJl5gMtHON7TQ=='),
+	'container': os.getenv('CONTAINER', 'new6'),
+    'dataset': os.getenv('DATASET', 'datasets'),
+    'ckan_host': os.getenv('CKAN_HOST', 'http://40.71.214.191:80'),
+    'ckan_key': os.getenv('CKAN_KEY', '3474fcd0-2ebc-4036-a60a-8bf77eea161f'),
+}

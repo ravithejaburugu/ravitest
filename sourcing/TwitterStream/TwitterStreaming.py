f2bfcbf070963d15be991f74af0cdb24e4ee4520
Ravi theja burugu <ravithejab@gmail.com>
2017-09-21 17:13:40 +0530

    Add files via upload

--- sourcing/TwitterStream/TwitterStreaming.py
+++ sourcing/TwitterStream/TwitterStreaming.py
@@ -11,6 +11,8 @@ import json
 import datetime
 import requests
 import re
+from config import argument_config
+from kafka import KafkaProducer
 from bs4 import BeautifulSoup
 
 
@@ -18,23 +20,46 @@ class TwitterStreamListener(tweepy.StreamListener):
     """This is the stream listener, resposible for listeneing from Twitter
     and receiving data using countinuously Streaming."""
 
+    def __init__(self, handle):
+        self.t = 60
+        self.handle = handle
+        kafka_broker_uri = 'localhost:9092'
+        self.producer = KafkaProducer(bootstrap_servers=[kafka_broker_uri])
+
     def on_data(self, response):
         """ This method is invoked on obtaining response data from the
         Twitter stream """
 
         # parsing response to JSON format
-        json_resp = json.loads(response+"\n")
+        json_resp = json.loads(response)
 
-        filename = "twitter_" + str(datetime.date.today()).replace('-', '') +\
-            ".json"
+        if (json_resp["retweeted"] is 'true') or ('RT @' in json_resp["text"]):
+            return True
 
         # Writing the Twitter response tweets to a file
+        filename = "twitter_" + str(datetime.date.today()).replace('-', '') +\
+            ".json"
         with open(filename, 'a+') as response_file:
             json.dump(json_resp, response_file, sort_keys=True, indent=4)
 
-        # show progress
         logging.info("Obtained a tweet from Twitter stream. \
                      CTRL+C to terminate the streaming.")
+
+        # Writing tweet to specific topic in Kafka.
+        screen_names = []
+        user_mentions = json_resp["entities"]["user_mentions"]
+        for user_mention in user_mentions:
+            if user_mention["screen_name"] in self.handle:
+                screen_name = user_mention["screen_name"]
+                screen_names.append(screen_name)
+
+        for account_name in screen_names:
+            filename = open(account_name+'.json', 'a+')
+            json.dump(json_resp, filename, sort_keys=True, indent=4)
+            # for custom consumer
+            self.producer.send(account_name, json.dumps(json_resp))
+            self.producer.flush()
+
         return True
 
     def on_error(self, status):
@@ -66,16 +91,10 @@ def main():
                         level=logging.INFO)
 
     # Authentication details as arguments.
-    azure_account_name = argument_config.get('azure_account_name')
-    azure_account_key = argument_config.get('azure_account_key')
-    azure_container = argument_config.get('azure_container')
-    ckan_host = argument_config.get('ckan_host')
-    ckan_api_key = argument_config.get('ckan_key')
-
-    consumer_key = "sCmEgB3NYwt4Pnolar1CgDqNj"
-    consumer_secret = "mllcum0wjpb6KdyHV0lqJRzH6FSgpDVB3MSzNQMOx9wKt3cSae"
-    access_token = "909762256350601216-eqFBd8WIW7PmHsru3VlR6eS7Mg5dnXT"
-    access_token_secret = "uTjwzMo7jEfkRVWyA9dSJAQ6mePBGEcmjgCVa0lAIDB23"
+    consumer_key = argument_config.get('consumer_key')
+    consumer_secret = argument_config.get('consumer_secret')
+    access_token = argument_config.get('access_token')
+    access_token_secret = argument_config.get('access_token_secret')
 
     listener = TwitterStreamListener()
 
---
+++ sourcing/TwitterStream/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 16:48:44 2017
+
+@author: ADMIN
+"""
+
+import os
+
+
+argument_config = {
+    'consumer_key': os.getenv('CONSUMER_KEY', 'sCmEgB3NYwt4Pnolar1CgDqNj'),
+    'consumer_secret': os.getenv('CONSUMER_SECRET', 'mllcum0wjpb6KdyHV0lqJRzH6FSgpDVB3MSzNQMOx9wKt3cSae'),
+    'access_token': os.getenv('ACCESS_TOKEN', '909762256350601216-eqFBd8WIW7PmHsru3VlR6eS7Mg5dnXT'),
+    'access_token_secret': os.getenv('ACCESS_TOKEN_SECRET', 'uTjwzMo7jEfkRVWyA9dSJAQ6mePBGEcmjgCVa0lAIDB23'),
+}

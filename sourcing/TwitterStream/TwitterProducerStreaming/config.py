22c85c3d38411477f2c11ba06ce3bae93bedf86d
Anshul Ghogre <30301360+aghogre@users.noreply.github.com>
2017-10-04 23:27:01 +0530

    Update config.py

--- sourcing/TwitterStream/TwitterProducerStreaming/config.py
+++ sourcing/TwitterStream/TwitterProducerStreaming/config.py
@@ -20,10 +20,10 @@ HASHTAGS = ["business", "WSJ", "WSJMarkets", "Forbes", "TheEconomist", "FT",
             "jessefelder", "DailyFXTeam", "Ukarlewitz", "Stephanie_Link"]
 
 argument_config = {
-    'consumer_key': os.getenv('CONSUMER_KEY', '2a3ZB5yDRj1KsRVN46ScMfjMM'),
-    'consumer_secret': os.getenv('CONSUMER_SECRET', 'MBhC08qXb2ooynexboPWNdVCVzYnWfUl0IZCXQzHTDNDXII2hK'),
-    'access_token': os.getenv('ACCESS_TOKEN', '912282441276055552-8ZR1KVdWtbNzlgHkQlPzQtYJqgGrOZd'),
-    'access_token_secret': os.getenv('ACCESS_TOKEN_SECRET', 'X2axJM3ETUfu2wEBjl2XWZ5Dr9yy5j3luwm4FkWWJFRun'),
+    'consumer_key': os.getenv('CONSUMER_KEY', ''),
+    'consumer_secret': os.getenv('CONSUMER_SECRET', ''),
+    'access_token': os.getenv('ACCESS_TOKEN', ''),
+    'access_token_secret': os.getenv('ACCESS_TOKEN_SECRET', ''),
     'twitter_hashtags': os.getenv('TWITTER_HASHTAGS', HASHTAGS),
     'kafka_broker_uri': os.getenv('KAFKA_BROKER_URI', 'localhost:9092'),
     'kafka_topic': os.getenv('KAFKA_TOPIC', 'twitterhandle')

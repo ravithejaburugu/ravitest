08f13d42140f837d376c12df9b39daa2b7ab65b7
hchaluvadiRT <hchaluvadi@randomtrees.com>
2017-10-20 16:09:16 +0530

    Update SiteMap.py

--- sourcing/FinancialNewsData/SiteMap.py
+++ sourcing/FinancialNewsData/SiteMap.py
@@ -34,26 +34,27 @@ class SitemapParser():
         # Proces each .xml urls from robots.txt and return html object.
         index_response = requests.get(sitemap_url)
         index_root = etree.fromstring(index_response.content)
-        print "The number of xml urls {0}".format(len(index_root))
-
+        logging.info(":: The number of urls {0}".format(len(index_root)) +
+                     " in {0}".format(source))
         # Process  all sub .xml urls available,stored in list
         h = 1
         for sitemap in index_root:
             index_children = sitemap.getchildren()
             index_loc = index_children[0].text
             if index_loc.split(".")[-1] == "xml":
-                print "Processing the {0} url ".format(h) + "amoung {0} Urls"\
-                    .format(len(index_root))
+                logging.info(":: Processing the {0} url ".format(h) +
+                             " amoung {0} Urls".format(len(index_root)))
                 urlset_response = requests.get(index_loc)
                 soup = BeautifulSoup(urlset_response.text)
                 urlset_locs = soup.find_all("loc")
-                print " Processing the {0} url and it contains".format(h) \
-                    + "{0} urls" .format(len(urlset_locs))
+                logging.info(":: Processing the {0} url and it contains".
+                             format(h) + " {0} urls" .format(len(urlset_locs)))
                 h = h + 1
                 k = 1
                 # Process all sub .xml urls list,returns html object for mongo
                 for urlset_loc in urlset_locs:
                     final_url = urlset_loc.contents[0]
+                    logging.info(":: {0} url is inserted into mongo".format(k))
                     http = httplib2.Http()
                     http_headers, http_response = http.request(final_url,
                                                                'GET')
@@ -73,31 +74,27 @@ class SitemapParser():
         sitemap_xml = self.decompress_stream(response.raw)
         tree = etree.parse(sitemap_xml)
         root = tree.getroot()
-        print "The number of gz tags are {0}".format(len(root))
+        logging.info(" ::The number of zip urls are {0}".format(len(root)))
         locs = []
-        i = 1
         for sitemap in root:
                 children = sitemap.getchildren()
-                print i, children[0].text
-                i = i + 1
-                type(children)
                 locs.append(children[0].text)      # 23 urls
-                len(locs)
 
         j = 1
         for loc in locs:
             if loc.split(".")[-1] == "gz":
-                print j, " The procesing url is {0} ".format(loc)
-                response = requests.get(loc, stream=True)
-                sitemap_xml = self.decompress_stream(response.raw)
-                tree = etree.parse(sitemap_xml)
-                root = tree.getroot()
-                print " <<<>>>> Final_urls are {0}".format(len(root)) + \
-                    " at {0} ".format(loc.split("/")[-1])
-                j = j + 1
+                logging.info(" ::Processing {0} url".format(j) +
+                             " amoung {}".format(len(root)))
+                response1 = requests.get(loc, stream=True)
+                sitemap_xml1 = self.decompress_stream(response1.raw)
+                tree1 = etree.parse(sitemap_xml1)
+                root1 = tree1.getroot()
+                logging.info(" ::Processing {0} url".format(j) +
+                             " Contains {0} ".format(len(root1)) + "urls")
 
+                j = j + 1
                 final_urls = []
-                for sitemap in root:
+                for sitemap in root1:
                     children = sitemap.getchildren()
                     final_urls.append(children[0].text)
 
@@ -107,7 +104,8 @@ class SitemapParser():
                     http_headers, http_response =  \
                         http.request(final_url, 'GET')
                     if http_headers['status'] == "200":
-                        print " Inserted {0} url into mongo  ".format(k)
+                        logging.info(" ::{0} urls Inserted  into mongo"
+                                     .format(k))
                         k = k+1
                         self.kafkaSendProducer(source, http_response)
 

70e5dfc011ced14085aac9c5d39fa659f0353c91
Anshul Ghogre <30301360+aghogre@users.noreply.github.com>
2017-10-28 13:30:02 +0530

    Update config.py

--- sourcing/FinancialNewsData/FinancialNewsDataConsumer/config.py
+++ sourcing/FinancialNewsData/FinancialNewsDataConsumer/config.py
@@ -38,7 +38,7 @@ SOURCES = [
         ]
 
 argument_config = {
-    'kafka_broker_uri': os.getenv('KAFKA_BROKER_URI', 'localhost:9092'),
+    'kafka_broker_uri': os.getenv('KAFKA_BROKER_URI', '173.193.179.253:9097'),
     'kafka_topics': os.getenv('KAFKA_TOPICS', SOURCES),
     'ckan_host': os.getenv('CKAN_HOST', 'http://40.71.214.191:80'),
     'api_key': os.getenv('CKAN_API_KEY', '3474fcd0-2ebc-4036-a60a-8bf77eea161f'),
@@ -49,10 +49,10 @@ mongo_config = {
     'mongo_uri': os.getenv('MONGO_URI', 'localhost:27017'),
     'ssl_required': os.getenv('MONGO_SSL_REQUIRED', False),
     'requires_auth': os.getenv('REQUIRES_AUTH', 'false'),
-    'mongo_username': os.getenv('MONGO_USER', 'ravithejab@gmail.com'),
-    'mongo_password': os.getenv('MONGO_PASSWORD', 'sl03pois!'),
+    'mongo_username': os.getenv('MONGO_USER', ''),
+    'mongo_password': os.getenv('MONGO_PASSWORD', ''),
     'mongo_auth_source': os.getenv('MONGO_AUTH_SOURCE', 'dbadmin'),
     'mongo_auth_mechanism': os.getenv('MONGO_AUTH_MECHANISM', 'MONGODB-CR'),
-    'db_name': os.getenv('MONGO_DB_NAME', 'finnews_all2'),
+    'db_name': os.getenv('MONGO_DB_NAME', 'fin'),
     'mongo_index_name': os.getenv('MONGO_INDEX_NAME', 'csrt'),
 }

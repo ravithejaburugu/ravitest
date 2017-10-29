a46d70f6836a24c252d54dcd96f674db45b3faca
Ravi theja burugu <ravithejab@gmail.com>
2017-10-29 20:31:14 +0530

    Update FinancialDataExtractor.py

--- sourcing/FinancialNewsData/FinancialNewsDataProducer/FinancialDataExtractor.py
+++ sourcing/FinancialNewsData/FinancialNewsDataProducer/FinancialDataExtractor.py
@@ -20,7 +20,7 @@ from finsymbols.symbols import get_sp500_symbols, get_nyse_symbols
 from finsymbols.symbols import get_amex_symbols, get_nasdaq_symbols
 from FinKafkaProducer import finKafkaProducer
 from requests.auth import HTTPBasicAuth
-from mongoDBConnection import initialize_mongo
+from mongoDBConnection import initialize_mongo, insert_into_mongo
 
 kafkaProducer = finKafkaProducer()
 mongo_colln = initialize_mongo()
@@ -47,89 +47,73 @@ def main():
     ft_api_id = argument_config.get('ft_api_id')
     ft_api_pwd = argument_config.get('ft_api_pwd')
 
-    # while True:
-    """ RSSFeedParsing begins."""
-    for rss_feed_name in rss_feed_urls:
-        rss_feed_url = rss_feed_urls[rss_feed_name]
-        logging.info("RSSfeed " + rss_feed_name + "[" + rss_feed_url + "]")
-        parseAndSave(rss_feed_name, rss_feed_url)
-
-    """URLs with subscription using authentication."""
-    if ft_api_id and ft_api_pwd:
-        try:
-            news_url = "http://api.ft.com/content/search/v1"
-            scrapeFT("financial-times", news_url, ft_api_id, ft_api_pwd,
-                     ft_api_key)
-        except:
-            logging.warn("Problem while scraping Financial Times")
-    else:
-        logging.warn("Subscription not found for Financial Times")
-
-    """if wsj_api_id and wsj_api_pwd:
-        try:
-            news_url = "https://newsapi.org/v1/articles"
-            ## scrapeWSJ(auth_item, news_url, auth_id, auth_pwd)
-        except:
-            logging.warn("Problem while scraping WSJ")
-    else:
-        logging.warn("Subscription not found for Wall Street Journal.")"""
-
-    """ Scraping begins."""
-    for source in scrapy_urls:
-        scrape_url = scrapy_urls[source]
-        logging.info("RSS feed of " + source + "[" + scrape_url + "]")
-        scrapeAndSave(source, scrape_url)
-
-    """ Historical data scraping using sitemap url. """
-    for source in historical_urls:
-        # Check if historical data is already downloaded.
-        metadata_cursor = mongo_colln.find({"HISTORICAL": source})
-        if metadata_cursor.count() == 0:
-            robots_url = historical_urls[source]
+    while True:
+        """ RSSFeedParsing begins."""
+        for rss_feed_name in rss_feed_urls:
+            rss_feed_url = rss_feed_urls[rss_feed_name]
+            logging.info("RSSfeed " + rss_feed_name + "[" + rss_feed_url + "]")
+            parseAndSave(rss_feed_name, rss_feed_url)
+    
+        """URLs with subscription using authentication."""
+        if ft_api_id and ft_api_pwd:
+            try:
+                news_url = "http://api.ft.com/content/search/v1"
+                scrapeFT("financial-times", news_url, ft_api_id, ft_api_pwd,
+                         ft_api_key)
+            except:
+                logging.warn("Problem while scraping Financial Times")
+        else:
+            logging.warn("Subscription not found for Financial Times")
+    
+        """ Scraping begins."""
+        for source in scrapy_urls:
+            scrape_url = scrapy_urls[source]
+            logging.info("RSS feed of " + source + "[" + scrape_url + "]")
+            scrapeAndSave(source, scrape_url)
+    
+        """ Historical data scraping using sitemap url. """
+        for source in historical_urls:
+            # Check if historical data is already downloaded.
+            metadata_cursor = mongo_colln.find({"HISTORICAL": source})
+            if metadata_cursor.count() == 0:
+                robots_url = historical_urls[source]
+                logging.info("SiteMap for " + source + " from " + robots_url)
+                crawlAndScrape(source, robots_url)
+    
+                meta_mongo_colln = initialize_mongo("METADATA")
+                meta_feedObj = {"METADATA": source}
+                insert_into_mongo(meta_mongo_colln, meta_feedObj)
+    
+            else:
+                logging.info("Historical data already downloaded for " + source)
+    
+        """ SitemapParsing begins. It fetches all sitmap urls from robots.txt and
+            filters the zip, xml urls."""
+        for source in site_map_urls:
+            robots_url = site_map_urls[source]
             logging.info("SiteMap for " + source + " from " + robots_url)
             crawlAndScrape(source, robots_url)
-        else:
-            logging.info("Historical data already downloaded for " + source)
+    
+        all_fin_symbols = get_sp500_symbols() + get_nyse_symbols() + get_amex_symbols() + get_nasdaq_symbols()
 
-    """ SitemapParsing begins. It fetches all sitmap urls from robots.txt and
-        filters the zip, xml urls."""
-    for source in site_map_urls:
-        robots_url = site_map_urls[source]
-        logging.info("SiteMap for " + source + " from " + robots_url)
-        crawlAndScrape(source, robots_url)
+        """ Google finance News extraction begins."""
+        logging.info("Google Finance News...")
+        getGoogleNews("google_fin_news", all_fin_symbols)
 
-    all_fin_symbols = get_sp500_symbols() + get_nyse_symbols() + get_amex_symbols() + get_nasdaq_symbols()
+        """ Google Stocks extraction begins."""
+        logging.info("Google Live Stocks...")
+        getGoogleQuotes("google_stocks", all_fin_symbols)
 
-    """ Google finance News extraction begins."""
-    logging.info("Google Finance News...")
-    getGoogleNews("google_fin_news", all_fin_symbols)
+        """Yahoo finance stock prices."""
+        logging.info("Yahoo Finance Stocks...")
+        getYahooStocks("yahoo_stocks", all_fin_symbols)
 
-    """ Google Stocks extraction begins."""
-    logging.info("Google Live Stocks...")
-    getGoogleQuotes("google_stocks", all_fin_symbols)
-
-    """Yahoo finance stock prices."""
-    logging.info("Yahoo Finance Stocks...")
-    getYahooStocks("yahoo_stocks", all_fin_symbols)
-
-    logging.info(feed_count)
-    logging.info("Total time taken for extracting all Financial News Data :: "
-                 + str(time.time() - t1))
-        #time.sleep(180)
-
-"""
-def scrapeWSJ(feedName, news_url, auth_id, auth_pwd):
-    querystring = {"source": feedName,
-                   "sortBy": "top",
-                   "apiKey": "67c7663f07af4c388e2734118ffa6b17"}
-    auth_response = requests.request("GET", news_url, params=querystring)
-    js = json.loads(auth_response.content)
-    for url in js['articles']:
-        metadata_cursor = mongo_colln.find({feedName: url['url']})
-        if metadata_cursor.count() == 0:
-            sendToKafka(feedName, url['url'], auth_id, auth_pwd)
-        else:
-            logging.info("Duplicate data Skipped")"""
+        logging.info(feed_count)
+                logging.info("Total time taken for extracting all Financial News Data "
+                     + ":: " + str(time.time() - t1))
+        logging.info("This program is now in sleep mode. Will re-execute " +
+                     "after 30 mins for latest news feeds.")
+        time.sleep(180)
 
 
 def scrapeFT(feedName, news_url, auth_id, auth_pwd, ft_api_key):
@@ -171,29 +155,21 @@ def crawlAndScrape(source, robots_url):
             print sitemap_url
             logging.info("Extracting SiteMap: " + sitemap_url)
 
-            # for XML formats in Sitemap
-            if sitemap_url.split(".")[-1] == "xml":
-                cnt = sitemapParser.crawlSiteMapXML(source, sitemap_url)
-                if source in feed_count:
-                    feed_count[source] = feed_count[source] + cnt
-                else:
-                    feed_count[source] = cnt
-
             # for gz formats in Sitemap
-            elif sitemap_url.split(".")[-1] == "gz":
+            if sitemap_url.split(".")[-1] == "gz":
                 cnt = sitemapParser.unzipSiteMapURL(source, sitemap_url)
                 if source in feed_count:
                     feed_count[source] = feed_count[source] + cnt
                 else:
                     feed_count[source] = cnt
 
-            # for HTML formats in Sitemap
-            elif sitemap_url.split(".")[-1] == "html":
-                pass
-
-            # for other unknown formats
+            # for all other formats
             else:
-                pass
+                cnt = sitemapParser.crawlSiteMapXML(source, sitemap_url)
+                if source in feed_count:
+                    feed_count[source] = feed_count[source] + cnt
+                else:
+                    feed_count[source] = cnt
 
 
 def scrapeAndSave(feedName, feedURL):

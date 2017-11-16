e25777c135d70e02b95b813c4e257a76f24df07b
Ravi theja burugu <ravithejab@gmail.com>
2017-11-16 13:15:55 +0530

    Update config.py

--- sourcing/Quandl/config.py
+++ sourcing/Quandl/config.py
@@ -23,7 +23,7 @@ mongo_config = {
     'mongo_password': os.getenv('MONGO_PASSWORD', ''),
     'mongo_auth_source': os.getenv('MONGO_AUTH_SOURCE', 'dbadmin'),
     'mongo_auth_mechanism': os.getenv('MONGO_AUTH_MECHANISM', 'MONGODB-CR'),
-    'db_name': os.getenv('MONGO_DB_NAME', 'quan7'),
+    'db_name': os.getenv('MONGO_DB_NAME', 'quan8'),
     'mongo_index_name': os.getenv('MONGO_INDEX_NAME', '_hash'),
     'col_name': os.getenv('MONGO_COL_NAME', 'METADATA'),
 }

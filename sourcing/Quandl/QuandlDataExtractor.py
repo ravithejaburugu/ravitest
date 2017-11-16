12cac0a8c06791cf9e1eb266b44d9a50b7450ad5
Ravi theja burugu <ravithejab@gmail.com>
2017-11-16 16:47:57 +0530

    Add files via upload

--- sourcing/Quandl/QuandlDataExtractor.py
+++ sourcing/Quandl/QuandlDataExtractor.py
@@ -27,16 +27,19 @@ def main():
     # Fetching URLs from CSV file.
     with open('Quandl_csv1.csv', 'r') as csv_file:
         for line in csv_file.readlines():
-            last_ele = filter(None, reversed(map(str.strip, line.split(','))))[0]
+            rev_eles = filter(None, reversed(map(str.strip, line.split(','))))
+            last_ele = ''
+            if len(rev_eles) > 0:
+                last_ele = rev_eles[0]
             if last_ele.startswith('http'):
                 dataset_url = last_ele + quandl_apikey
-
                 dataset_source = line.split(',')[0]
                 logging.info("Fetching data of : " + dataset_source)
                 try:
                     time.sleep(3)
                     resp_data = os.popen("curl " + dataset_url).read()
                     json_data = json.loads(resp_data)
+                    logging.info(json_data)
 
                     dataset_source = dataset_source.strip().replace(' ', '_')\
                                                            .replace('\"', '')
